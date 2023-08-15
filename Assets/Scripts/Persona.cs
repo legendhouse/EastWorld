7182319793af245bcb6803e9cdd9f8972757b513
lee <529594158@qq.com>
2023-08-16 01:06:10 +0800

    代理人物类

---
+++ Assets/Scripts/Persona.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persona : MonoBehaviour, ICognition
+{
+    #region MonoBehaviour
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+
+
+    #region ICognition Implements
+    public void Perceive()
+    {
+
+    }
+
+    public void Retrieve()
+    {
+
+    }
+
+    public void Plan()
+    {
+
+    }
+
+    public void Excecute()
+    {
+
+    }
+
+    public void Reflect()
+    {
+
+    }
+    #endregion
+
+    #region IO
+    /// <summary>
+    /// 保存任务当前状态(序列化)
+    /// </summary>
+    public void Save()
+    {
+
+    }
+
+    /// <summary>
+    /// 加载人物状态(反序列化)
+    /// </summary>
+    public void Load()
+    {
+
+    }
+    #endregion
+
+    #region Action
+    /// <summary>
+    /// 与其他人物进行对话
+    /// </summary>
+    public void StartConversationSession()
+    {
+
+    }
+
+    /// <summary>
+    /// 移动
+    /// </summary>
+    public void Move()
+    {
+
+    }
+    #endregion
+}
