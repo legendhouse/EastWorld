b2508ec14e0912dff9eed8495bca1ec477250a96
lee <529594158@qq.com>
2023-09-01 01:15:03 +0800

    大模型相关
    Signed-off-by: lee <529594158@qq.com>

---
+++ Assets/Scripts/LLM/OpenAiApiTester.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class OpenAiApiTester : MonoBehaviour
+{
+    [Header("Input")]
+    public string chatInput;
+    public string embeddingInput;
+
+    [Header("Output")]
+    [TextArea]
+    public string chatOutput;
+    [TextArea]
+    public string embeddingOutput;
+}
