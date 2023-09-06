3bcff0cd6d8fbf62f9675a2835e5ef9ba92ce7bc
lee <529594158@qq.com>
2023-09-07 00:49:28 +0800

    pc 还原memory相关类

---
+++ Assets/Scripts/Memory/MemoryStream.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+public class MemoryStream
+{
+    /// <summary>
+    /// 从周围observe的或自己身上发生的事件记忆
+    /// </summary>
+    [JsonProperty(PropertyName = "eventDict")]
+    public Dictionary<string, BaseNode> EventDict { get; set; } = new Dictionary<string, BaseNode>();
+
+    /// <summary>
+    /// 主要是reflection获得的高层记忆
+    /// </summary>
+    [JsonProperty(PropertyName = "thoughtDict")]
+    public Dictionary<string, BaseNode> ThoughtDict { get; set; } = new Dictionary<string, BaseNode>();
+
+    /// <summary>
+    /// 与周围人聊天的记录，key为对方人名，value为聊天内容node
+    /// </summary>
+    [JsonProperty(PropertyName = "chatDict")]
+    public Dictionary<string, string> ChatDict { get; set; } = new Dictionary<string, string>();
+}
